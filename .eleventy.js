9d6f19efcad029be26f9f818f1c5678bff5312dc
evanostroski <eostroski@gmail.com>
2020-05-15 18:01:31 -0700

    bootstrap, parcel, ignore files and more

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function(eleventyConfig) {
-    // Output directory: _site
-  
-    // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy("img");
+  // Output directory: _site
 
-    // Copy `css/` to `_site/css`
-    eleventyConfig.addPassthroughCopy("css");
-  };
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("img");
+
+  // Copy `css/` to `_site/css`
+  eleventyConfig.addPassthroughCopy("css");
+
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+};
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # front-sandbox
 
-Checkout the wiki for Jason's notes: https://github.com/evanostroski/front-sandbox/wiki
+- Checkout the wiki for Jason's notes: https://github.com/evanostroski/front-sandbox/wiki
+- https://stevepolito.design/blog/create-a-bootstrap-starter-theme-in-eleventy/
+- https://www.11ty.dev/docs/plugins/navigation/
