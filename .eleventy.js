9f08971c769120234d82fa9e1384954ad7ba9fb5
Evan Ostroski <eostroski@gmail.com>
2025-04-04 12:04:53 -0400

    Add parcel dev dependency

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,9 @@
-module.exports = function(eleventyConfig) {
-  // Output directory: _site
+require("./.eleventy.css");
+require("bootstrap");
 
-  // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("/assets/img");
-  eleventyConfig.addWatchTarget("/assets/img");
+// Bootstrap JS and CSS
 
-  // Copy `css/` to `_site/css`
-  eleventyConfig.addPassthroughCopy("/assets/css");
-  eleventyConfig.addWatchTarget("/assets/css");
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
-  eleventyConfig.addLayoutAlias('form', 'layouts/form.njk');
-  eleventyConfig.addLayoutAlias('chat', 'layouts/chat.njk');
-  eleventyConfig.addLayoutAlias('plugin', 'layouts/plugin.njk');
-  eleventyConfig.addLayoutAlias('intercom', 'layouts/intercom.njk');
-};
+
+
+//# sourceMappingURL=.eleventy.js.map
